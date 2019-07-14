3b36027d6de9bc059fd3713a0b26132f340a84e7
Leandro Iadanza <leandrojulian1391@gmail.com>
2019-07-14 19:35:30 -0300

    Last version stable with angular and webpack

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,6 +4,19 @@ import appComponent from './app.component';
 import ComponentsModule from './components/components';
 
 
-angular.module('app', [
-  ComponentsModule.name
-]).component('app', appComponent);
+const myApp = angular.module('app', [ComponentsModule.name]).component('app', appComponent);
+
+myApp.controller('controlVote', ['$scope' ,function($scope) {
+
+  $scope.Carolina = 1;
+  $scope.Mario = 1;
+  $scope.Leonardo = 1;
+  $scope.Joaquin = 1;
+
+  $scope.Guardar = function() {
+    console.log("estoy en el control votar");
+
+    
+  }
+
+}])
