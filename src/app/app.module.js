cac6aa441663159f17fd99bcb80066a92b13c5b7
Leandro Iadanza <leandrojulian1391@gmail.com>
2019-07-29 17:15:05 -0300

    First dApp with AngugarJS WORKS!

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,12 +4,15 @@ import appComponent from './app.component';
 import ComponentsModule from './components/components';
 import getWeb3 from './app.getWeb3';
 import VotacionContract from "./votacion";
+import { votacionService } from "./votacionService";
 
 
 const myApp = angular.module('app', [ComponentsModule.name]).component('app', appComponent);
 
-myApp.controller('controlVote', ['$scope' ,'notify', async function($scope, notify) {
-  let voteCarol = 0 , voteMario = 0 , voteLeo = 0 , VoteJoa = 0;
+// myApp.constant('web3', getWeb3());
+
+myApp.controller('controlVote', ['$scope', async function($scope) {
+  // let voteCarol = 0 , voteMario = 0 , voteLeo = 0 , VoteJoa = 0;
   let account = undefined;
 
   $scope.candidates = [
@@ -19,77 +22,80 @@ myApp.controller('controlVote', ['$scope' ,'notify', async function($scope, noti
     {name : "Joaquin"}
   ];
 
-  $scope.Carolina = voteCarol;
-  $scope.Mario = voteMario;
-  $scope.Leonardo = voteLeo;
-  $scope.Joaquin = VoteJoa;
+  $scope.Carolina = 0;
+  $scope.Mario = 0;
+  $scope.Leonardo = 0;
+  $scope.Joaquin = 0;
 
-  this.web3 = await getWeb3();
-  console.log("Version de web3 en la dependencia dev: " + this.web3.version);
-  this.votacion = await VotacionContract(this.web3.currentProvider);
-  console.log(this.votacion.voteForCandidate);
 
-  account = (await this.web3.eth.getAccounts())[0];
-  console.log("Cuenta 0: " + account);
+    let web3 = await getWeb3();
+    // console.log(this.web3.version);
+    let votacion = await VotacionContract(web3.currentProvider);
 
-  await this.votacion.voteForCandidate(this.web3.utils.asciiToHex('Carolina'), {from: account, value: this.web3.utils.toWei('2', 'ether')});
+  account = (await web3.eth.getAccounts())[0];
+  console.log("Cuenta 0 es: " + account);
+
+  // funciona
+  // await this.votacion.voteForCandidate(this.web3.utils.asciiToHex('Carolina'), {from: account, value: this.web3.utils.toWei('2', 'ether')});
   
+  $scope.Carolina = (await votacion.totalVotesFor(web3.utils.asciiToHex('Carolina'), {from: account})).toNumber();
+  $scope.Mario = (await votacion.totalVotesFor(web3.utils.asciiToHex('Mario'), {from: account})).toNumber();
+  $scope.Leonardo = (await votacion.totalVotesFor(web3.utils.asciiToHex('Leonardo'), {from: account})).toNumber();
+  $scope.Joaquin = (await votacion.totalVotesFor(web3.utils.asciiToHex('Joaquin'), {from: account})).toNumber();
+  $scope.$apply();
 
   $scope.Guardar = async function(candidato) {
 
     console.log("estoy en el control votar");
     console.log("Candidato elegido: " + candidato.name);
-
-    // await this.votacion.voteForCandidate(web3.utils.asciiToHex(candidato), account)
-    // debo realizar un servicio
-
-
-    switch(candidato.name) {
-      case 'Carolina':
-        voteCarol += 1;
-        break;
-      case "Mario":
-        voteMario += 1;
-        break;
-      case "Leonardo":
-        voteLeo += 1;
-        break;
-      case "Joaquin":
-        VoteJoa += 1;
-      default:
-        console.log("not choose in one case");
-    }
-
-    $scope.Carolina = voteCarol;
-    $scope.Mario = voteMario;
-    $scope.Leonardo = voteLeo;
-    $scope.Joaquin = VoteJoa;
     
-    notify(candidato);
+    // Estoy invocando el método votar candidato del contrato
+    await votacion.voteForCandidate(web3.utils.asciiToHex(candidato.name), {from: account, value: web3.utils.toWei('1', 'ether')});
+
+    //Llamo le método votos total del candidato seleccionado
+    $scope.Carolina = (await votacion.totalVotesFor(web3.utils.asciiToHex('Carolina'), {from: account})).toNumber();
+    $scope.Mario = (await votacion.totalVotesFor(web3.utils.asciiToHex('Mario'), {from: account})).toNumber();
+    $scope.Leonardo = (await votacion.totalVotesFor(web3.utils.asciiToHex('Leonardo'), {from: account})).toNumber();
+    $scope.Joaquin = (await votacion.totalVotesFor(web3.utils.asciiToHex('Joaquin'), {from: account})).toNumber();
+
+    // $scope.candidates.name = (await votacion.totalVotesFor(web3.utils.asciiToHex(candidato.name), {from: account})).toNumber();
+
+    $scope.$apply();
+
+    // switch(candidato.name) {
+    //   case 'Carolina':
+    //     voteCarol += 1;
+    //     break;
+    //   case "Mario":
+    //     voteMario += 1;
+    //     break;
+    //   case "Leonardo":
+    //     voteLeo += 1;
+    //     break;
+    //   case "Joaquin":
+    //     VoteJoa += 1;
+    //     break;
+    //   default:
+    //     console.log("not choose in one case");
+    // }
+
+    // $scope.Carolina = voteCarol;
+    // $scope.Mario = voteMario;
+    // $scope.Leonardo = voteLeo;
+    // $scope.Joaquin = VoteJoa;
+    
+    // notify(candidato);
 
   }
 
 
 }]);
 
-myApp.factory('notify', ['$window', function(win) {
-  
-  return function(msg) {
-      // web3 = await getWeb3();
-      win.alert("Se ha realizado un voto al candidato/a: " + msg.name);
-      // var account = (await this.web3.eth.getAccounts())[0];
-      // console.log(account);
-  }
+
+
+myApp.factory('notify', ['$window', async function(win) {
 
 }]);
-
-// myApp.service('serviceVotacion', ['constructor', async function(VotacionContract) {
-
-//   this.votarCandidato = await VotacionContract.voteForCandidate(candidate, { from, value });
-//     console.log("estoy en el factory serviceVotacion");;
-//   }
-
-// ]);
 
 // myApp.factory
 
--- src/app/votacionService.js
+++ src/app/votacionService.js
@@ -1,5 +1,5 @@
 
-export class VotacionService {
+export class votacionService {
     constructor(contract) {
         this.contract = contract;
     }
@@ -13,56 +13,3 @@ export class VotacionService {
     }
 
 }
-
-
-//     async buyFlight(flightIndex, from, value) {
-//         //es el objeto de metadatos de la transaccion lo que está entre {}
-//         return this.contract.buyFlights(flightIndex, { from, value });
-//     }
-
-//     async getFlights() {
-//         let total = await this.getTotalFlights();
-//         let flights = [];
-//         for(var i= 0; i < total; i++) {
-//             let flight = await this.contract.flights(i);
-//             flights.push(flight);
-//         }
-//         return this.mapFlights(flights);
-//     }
-
-//     async getCustomerFlights(account) {
-//         let totalFlights = await this.contract.getTotalFlightsCustomer(account);
-//         //console.log(totalFlights);
-//         let flights = [];
-//         for (var i = 0; i < totalFlights.toNumber(); i++) {
-//             let flight = await this.contract.customerFlights(account, i);
-//             flights.push(flight);
-//         }
-        
-//         return this.mapFlights(flights);
-//     }
-
-//     async getTotalFlights() {
-//         return (await this.contract.getTotalFlights()).toNumber();
-//     }
-
-//     async getRefundableEther(from) {
-//         return await this.contract.getRefundableEther({ from });
-//         //return loyaltyPoints.toNumber();
-//     }
-
-//     async redeemLoyaltyPoints(from) {
-//         return await this.contract.redeemLoyaltyPoints({ from });
-//     }
-
-//     //función de mapeo
-//     mapFlights(flights) {
-//         return flights.map(flight => {
-//             return {
-//                 name: flight[0],
-//                 price: flight[1].toNumber()
-//             }
-//         });
-//     }
-
-// }
